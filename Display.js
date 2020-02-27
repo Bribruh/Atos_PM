36c6194f4ee57431142586097b64081e8a111070
Bribruh <45540511+Bribruh@users.noreply.github.com>
2020-02-27 17:40:10 -0600

    Update Display.js

--- Display.js
+++ Display.js
@@ -16,6 +16,6 @@ app.set('view engine', 'ejs');
 //FOR MONGO
 
 
-mongoose.connect('mongodb://' + argv.be_ip + ':80/my_database');
+mongoose.connect('mongodb://127.0.0.1:80/?compressors=disabled&gssapiServiceName=mongodb');
 app.listen(8080, argv.fe_ip);
 console.log("App listening on port 8080");

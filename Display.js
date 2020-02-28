eb08e8a28a1b10a3d174030ef2f8128bc37df88f
Bribruh <45540511+Bribruh@users.noreply.github.com>
2020-02-27 20:59:58 -0600

    Update Display.js

--- Display.js
+++ Display.js
@@ -35,12 +35,12 @@ Project.
             myArray.push(p[i]);
             console.log(myArray[i]);
         }
-		
+	/*	
 	app.get('/', function (req, res) {
             //Passes data values from Mongo over to EJS file to be rendered
             res.render('Index', { name: myArray[0].name, number: myArray[0].Pnumber, id: myArray[0]._id});
         });
-	
+	*/
 	app.listen(8080);
 console.log("App listening on port 8080");
 	

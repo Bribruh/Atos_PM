ce3abc41c1ce5b834be622127608bc5f60b5aadb
Bribruh <45540511+Bribruh@users.noreply.github.com>
2020-03-05 16:09:19 -0600

    Update Connect.js

--- Connect.js
+++ Connect.js
@@ -10,19 +10,5 @@ mongoose.connect('mongodb://' + argv.be_ip + ':80/AtosDB');
 // define model =================
 	var Project = mongoose.model('Atos_P', {}, 'Atos_P');
 
-var myArray = [];
-
-Project.
-  find({}, function (err, p) {
-    if (err) return handleError(err);
-    for (var i = 0; i < p.length; i++){
-            //Stores every data point into myArray
-            myArray.push(p[i]);
-            console.log(myArray[i]);
-        }
-
-	
-
-	
-  });
+module.exports.Project = Project;
 

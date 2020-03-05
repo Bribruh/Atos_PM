3ebc0938ed608e6c555156acef32c4e969cabdde
AndyLiu <wxl160130@utdallas.edu>
2020-03-05 17:42:40 -0600

    testing

--- Display.js
+++ Display.js
@@ -1,40 +1,52 @@
-//FOR EXPRESS
+//CSV Parser
+const csv = require('csv-parser')
+const fs = require('fs')
+const results = [];
+
+//Imports column data from CSV stored in root folder
+fs.createReadStream('columnData.csv')
+    //CSV Header format
+    .pipe(csv(['headerName', 'field', 'sortable', 'filter']))
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+        //console.log(results);
+    });
 
 //Express: Configuration Details
 var express = require('express');
 var path = require("path");
 var app = express();
-var mongoose = require('mongoose'); 
-var morgan = require('morgan'); 
-var argv = require('optimist').argv;
-var Connect = require("./Connect");
 
 //Express: setting static path
 app.use(express.static(path.join(__dirname, 'views')));
 //Express: Changing the view enging
 app.set('view engine', 'ejs');
-app.use(morgan('dev')); 
-//FOR MONGO
 
+//Mongoose
+var mongoose = require('mongoose'); 
+var morgan = require('morgan'); 
+var argv = require('optimist').argv;
 
+//Executes connection into MongoDB and accesses AtosDB
+var Connect = require("./Connect");
 
-// define model =================
-	var Project = Connect.Project;
+//Defined model from MongoDB
+var Project = Connect.Project;
 
-var myArray = [];
+//Global array to store all mongo db data entries
+const myProjects = [];
 
-Project.
-  find({}, function (err, p) {
+Project.find({}, function (err, p) {
     if (err) return handleError(err);
     for (var i = 0; i < p.length; i++){
             //Stores every data point into myArray
-            myArray.push(p[i]);
-            console.log(myArray[i]);
+            myProjects.push(p[i]);
+            //console.log(myProjects[i]);
         }
 
 	app.get('/', function (req, res) {
             //Passes data values from Mongo over to EJS file to be rendered
-            res.render('Index', {myArray});
+            res.render('Index', {myArray: myProjects, myColumn: results});
         });
 	
 	
---
+++ README.txt
@@ -0,0 +1,12 @@
+Install Guide: 
+
+Morgan: npm install morgan --save
+Optimist: npm install optimist --save
+Mongoose: npm install mongoose --save
+Express: npm install express --save
+EJS: npm install ejs --save
+CSV-Parser: npm install csv-parser --save
+
+Root Folder:
+- TestData.json: User-generated test data stored inside of remote MongoDB
+- columData.csv: User-generated CSV document containing column data for Ag-Grid API

3f0d5425d427f358f9e885b9fb02a8f0b46fbd9c
Anthony <36807633+artomsio@users.noreply.github.com>
2020-03-07 14:42:53 -0600

    with node modules

--- Connect.js
+++ Connect.js
@@ -4,11 +4,14 @@ var morgan = require('morgan');
 var argv = require('optimist').argv;
 
 
-mongoose.connect('mongodb://' + argv.be_ip + ':80/AtosDB');
+//mongoose.connect('mongodb://' + argv.be_ip + ':80/AtosDB');
+mongoose.connect('mongodb://localhost:27017/ProjectDB');
 
 
 // define model =================
-	var Project = mongoose.model('AtosProjects', {}, 'AtosProjects');
+	//var Project = mongoose.model('AtosProjects', {}, 'AtosProjects');
+	var Project = mongoose.model('Project', {}, 'Project'); 
+
 
 module.exports.Project = Project;
 
